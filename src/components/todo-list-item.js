7be727fbcc25bd9dfb0661fd86656d980a680177
yuriipereverziev <yuripereverzev@gmail.com>
2022-08-03 19:21:27 +0300

    add: color style

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TodoListItem = () => {
-  return <span>Drink Coffee</span>;
+const TodoListItem = ({ label, important = false }) => {
+  console.log(important);
+
+  const style = {
+    color: important ? 'tomato' : 'black'
+  };
+
+  return <span style={style}>{ label }</span>;
+};
+
+TodoListItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  important: PropTypes.bool.isRequired,
 };
 
 export default TodoListItem;
