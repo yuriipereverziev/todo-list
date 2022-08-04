1a3bba919d8a8e8e9734929bf2e8ff02146fbda0
yuriipereverziev <yuripereverzev@gmail.com>
2022-08-04 17:42:40 +0300

    add: class component

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,42 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'tomato' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+export default class TodoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}
-      >
-        {label}
+    const style = {
+      color: important ? 'tomato' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}
+        >
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button
-        type="button"
-        className="btn btn-outline-success btn-sm float-right"
-      >
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button
-        type="button"
-        className="btn btn-outline-danger btn-sm float-right"
-      >
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+    );
+  }
+}
 
 TodoListItem.propTypes = {
   label: PropTypes.string.isRequired,
   important: PropTypes.bool.isRequired,
 };
-
-export default TodoListItem;
